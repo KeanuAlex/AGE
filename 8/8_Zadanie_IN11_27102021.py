ad68f1209048af4c51840546c0da2491949d2a29
KeanuAlex <KeanuAlex@yandex.ru>
2021-11-05 22:17:55 +0500

    Update 8_Zadanie_IN11_27102021.py

--- 8/8_Zadanie_IN11_27102021.py
+++ 8/8_Zadanie_IN11_27102021.py
@@ -1,11 +1,10 @@
-s = "автор"
-count = 1
+s = "аворт"
+count = 0
 for a in s:
     for b in s:
         for c in s:
             for d in s:
                 st = a + b + c + d
                 count += 1
-                if st =="вата":
-                    break
-print(count)
+                if st == "вата":
+                    print(count, st)
