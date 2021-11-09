f631604a805bd43da4f64508cf3f39598a3c57e4
KeanuAlex <KeanuAlex@yandex.ru>
2021-11-09 17:10:23 +0500

    1

---
+++ 5/5_Zadanie_IN11_27102021.py
@@ -0,0 +1,13 @@
+for N in range(1, 10000):
+    N = str(N)
+    summ1 = 0
+    summ2 = 0
+    for i in N:
+        if int(i) % 2 == 0:
+            summ1 += int(i)
+    for i in range(0, len(N)):
+        if (i + 1) % 2 == 0:
+            summ2 += int(N[i])
+    if abs(summ1 - summ2) == 13:
+        print(N)
+        break
